38a61f3c82af2a84fd97c389572312999df0de00
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-13 10:46:45 -0400

    Add .css(color, #545149); to if (this.value.length <= 140) { to change color from red to original color.

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     if (this.value.length <= 140) {
       $(".counter").html(this.value.length);
+      $(".counter").css("color", "#545149");
     } else {
       $(".counter").html("-" + (this.value.length - 140) );
       $(".counter").css("color", "red");
