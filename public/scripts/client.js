c793e761b1f8984898446e9a6d5cbf51b8838160
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-16 11:26:19 -0400

    Add createTweetElement and renderTweets functions to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,75 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  //Implement a createTweetElement function
+  const createTweetElement = function(tweet) {
+    //Create hard coded tweets
+    const $tweet = $(`<article>
+          <header>
+            <div class="tweetIcon">
+              <img src= "${tweet.user.avatars}" alt="user icon">
+              <p class="nameID">${tweet.user.name}</p>
+            </div>
+            <p class="handle">${tweet.user.handle}</p>
+          </header>
+          <div class="tweetDiv">
+            <p class="tweet">${tweet.content.text}</p>
+          </div>
+          <footer>
+            <div>
+              <p id="tweetDate">${tweet.created_at}</p>
+            </div>
+            <div>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>`);
+    return $tweet;
 
+  };
+
+
+
+  // const $tweet = createTweetElement(data);
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  };
+
+  renderTweets(data);
+
+});
