6df9f0c38b07ea96b7cbf228f9779b22f6c34266
Tom Spencer <thomas.spencer2034@gmail.com>
2023-10-26 08:41:28 -0400

    Update character counter to begin at 140 and count down.

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 //Event listener for when a character is inputted into the text area
   $('#tweet-text').on('input', function() {
     if (this.value.length <= 140) {
-      $(".counter").html(this.value.length);
+      $(".counter").html(140 - this.value.length);
       $(".counter").css("color", "#545149");
     } else {
       $(".counter").html("-" + (this.value.length - 140));
